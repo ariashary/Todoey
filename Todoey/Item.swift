e070a8b6f8c2800bd7893a62c5fb30ae145f889b
Muhammad Ashary <muh.ariashary@gmail.com>
2019-06-28 14:55:24 +0700

    Convert to MVC design pattern.

---
+++ Todoey/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Muhammad Ashary on 28/06/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
